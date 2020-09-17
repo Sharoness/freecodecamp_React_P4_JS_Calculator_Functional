eb77091de4bbee6d795bb7738ea1e7983ba41139
Sharoness <sharong1234@hotmail.com>
2020-09-17 14:39:09 +0100

    added onclick events: clear and clicknumber

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Button } from 'react-bootstrap';
 
 const App = () => {
-  return (
-    <div>
-    	<Button id="equals">=</Button>
-    	<Button id="zero">0</Button>
-    	<Button id="one">1</Button>
-    	<Button id="two">2</Button>
-    	<Button id="three">3</Button>
-    	<Button id="four">4</Button>
-    	<Button id="five">5</Button>
-    	<Button id="six">6</Button>
-    	<Button id="seven">7</Button>
-    	<Button id="eight">8</Button>
-    	<Button id="nine">9</Button>
-    	<Button id="add">+</Button>
-    	<Button id="subtract">-</Button>
-    	<Button id="multiply">*</Button>
-    	<Button id="divide">:</Button>
-    	<Button id="decimal">.</Button>
-    	<Button id="clear">clear</Button>
-    	<div id="display"></div>
-    </div>
+	const [display, setDisplay] = useState("0");
+	const [currentValue, setCurrentValue] = useState();
+
+	const clear = () => {
+		setDisplay("0");
+		setCurrentValue();
+	}
+
+	const clickNumber = (value) => {
+		return () => {
+			setDisplay(value);
+			setCurrentValue(value);
+		};
+	}
+
+
+  	return (
+    	<div>
+	    	<Button id="equals">=</Button>
+	    	<Button id="zero" onClick={(clickNumber("1"))}>0</Button>
+	    	<Button id="one" onClick={(clickNumber("1"))}>1</Button>
+	    	<Button id="two" onClick={clickNumber("2")}>2</Button>
+	    	<Button id="three" onClick={clickNumber("3")}>3</Button>
+	    	<Button id="four" onClick={clickNumber("4")}>4</Button>
+	    	<Button id="five" onClick={clickNumber("5")}>5</Button>
+	    	<Button id="six" onClick={clickNumber("6")}>6</Button>
+	    	<Button id="seven" onClick={clickNumber("7")}>7</Button>
+	    	<Button id="eight" onClick={clickNumber("8")}>8</Button>
+	    	<Button id="nine" onClick={clickNumber("9")}>9</Button>
+	    	<Button id="add">+</Button>
+	    	<Button id="subtract">-</Button>
+	    	<Button id="multiply">*</Button>
+	    	<Button id="divide">:</Button>
+	    	<Button id="decimal">.</Button>
+	    	<Button id="clear" onClick={clear}>clear</Button>
+	    	<div id="display">{display}</div>
+    	</div>
     );
 }
 
