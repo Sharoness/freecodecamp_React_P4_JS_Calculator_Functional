5a84bf5288ac945ecdefce3db5552e5f464a08f3
Sharoness <sharong1234@hotmail.com>
2020-10-08 10:57:05 +0100

    made lay-out better

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Display from './Display.jsx';
 import Pad from './Pad.jsx';
+import './Calculator.css';
 
 const Calculator = () => {
 	const [display, setDisplay] = useState("0");
@@ -8,7 +9,7 @@ const Calculator = () => {
 	const [operator, setOperator] = useState("");
 
   	return (
-    	<div>
+    	<div id="calculator">
 			<Display display={display} lastValue={lastValue} />
 	    	<Pad display={display} lastValue={lastValue} operator={operator} setDisplay={setDisplay} setLastValue={setLastValue} setOperator={setOperator} />
     	</div>
--- src/Pad.jsx
+++ src/Pad.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
 import './Pad.css';
 
 const Pad = ({display, lastValue, operator, setDisplay, setLastValue, setOperator}) => {
@@ -100,23 +99,23 @@ const Pad = ({display, lastValue, operator, setDisplay, setLastValue, setOperato
     
     return (
         <div className="gridContainer">
-            <Button id="zero" className="number" onClick={clickNumber("0")}>0</Button>
-            <Button id="one" className="number" onClick={clickNumber("1")}>1</Button>
-            <Button id="two" className="number" onClick={clickNumber("2")}>2</Button>
-            <Button id="three" className="number" onClick={clickNumber("3")}>3</Button>
-            <Button id="four" className="number" onClick={clickNumber("4")}>4</Button>
-            <Button id="five" className="number" onClick={clickNumber("5")}>5</Button>
-            <Button id="six" className="number" onClick={clickNumber("6")}>6</Button>
-            <Button id="seven" className="number" onClick={clickNumber("7")}>7</Button>
-            <Button id="eight" className="number" onClick={clickNumber("8")}>8</Button>
-            <Button id="nine" className="number" onClick={clickNumber("9")}>9</Button>
-            <Button id="add" className="operator" onClick={operatorHandler("+")}>+</Button>
-            <Button id="subtract" className="operator" onClick={operatorHandler("-")}>-</Button>
-            <Button id="multiply" className="operator" onClick={operatorHandler("*")}>*</Button>
-            <Button id="divide" className="operator" onClick={operatorHandler("/")}>/</Button>
-            <Button id="decimal" className="number" onClick={decimal(".")}>.</Button>
-            <Button id="equals" onClick={equals(operator)}>=</Button>
-            <Button id="clear" onClick={clear}>clear</Button>
+            <button id="zero" className="number" onClick={clickNumber("0")}>0</button>
+            <button id="one" className="number" onClick={clickNumber("1")}>1</button>
+            <button id="two" className="number" onClick={clickNumber("2")}>2</button>
+            <button id="three" className="number" onClick={clickNumber("3")}>3</button>
+            <button id="four" className="number" onClick={clickNumber("4")}>4</button>
+            <button id="five" className="number" onClick={clickNumber("5")}>5</button>
+            <button id="six" className="number" onClick={clickNumber("6")}>6</button>
+            <button id="seven" className="number" onClick={clickNumber("7")}>7</button>
+            <button id="eight" className="number" onClick={clickNumber("8")}>8</button>
+            <button id="nine" className="number" onClick={clickNumber("9")}>9</button>
+            <button id="add" className="operator" onClick={operatorHandler("+")}>+</button>
+            <button id="subtract" className="operator" onClick={operatorHandler("-")}>-</button>
+            <button id="multiply" className="operator" onClick={operatorHandler("*")}>*</button>
+            <button id="divide" className="operator" onClick={operatorHandler("/")}>/</button>
+            <button id="decimal" className="number" onClick={decimal(".")}>.</button>
+            <button id="equals" onClick={equals(operator)}>=</button>
+            <button id="clear" onClick={clear}>AC</button>
         </div>
     )
 }
