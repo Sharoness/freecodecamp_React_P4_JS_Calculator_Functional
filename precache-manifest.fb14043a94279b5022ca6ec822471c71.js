3653179d84fd72892e746a00845987097ab7c4fa
Sharoness <sharong1234@hotmail.com>
2020-10-23 10:58:27 +0100

    Updates

---
+++ precache-manifest.fb14043a94279b5022ca6ec822471c71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55185805cc779ef913b9211d1e846551",
+    "url": "/freecodecamp_React_P4_JS_Calculator_Functional/index.html"
+  },
+  {
+    "revision": "998cb00608c9a211bc43",
+    "url": "/freecodecamp_React_P4_JS_Calculator_Functional/static/css/main.a79a753c.chunk.css"
+  },
+  {
+    "revision": "7497533f14bc55052392",
+    "url": "/freecodecamp_React_P4_JS_Calculator_Functional/static/js/2.d74c26b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/freecodecamp_React_P4_JS_Calculator_Functional/static/js/2.d74c26b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "998cb00608c9a211bc43",
+    "url": "/freecodecamp_React_P4_JS_Calculator_Functional/static/js/main.b521fa6b.chunk.js"
+  },
+  {
+    "revision": "4d980b1306f05f70cd54",
+    "url": "/freecodecamp_React_P4_JS_Calculator_Functional/static/js/runtime-main.affea776.js"
+  }
+]);
